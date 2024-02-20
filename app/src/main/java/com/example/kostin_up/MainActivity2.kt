993252ce537949f3bf1e7897de86e834865efbf4
Adverseir <128318858+Adverseir@users.noreply.github.com>
2024-02-20 15:04:52 +0300

    Messanger

--- app/src/main/java/com/example/kostin_up/MainActivity2.kt
+++ app/src/main/java/com/example/kostin_up/MainActivity2.kt
@@ -3,19 +3,20 @@ package com.example.kostin_up
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
 import android.widget.ImageButton
 
-class MainActivity : AppCompatActivity() {
+class MainActivity2 : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        setContentView(R.layout.activity_main2)
+        var Button : Button = findViewById(R.id.button)
+        Button.setOnClickListener{
 
-        var imageButton : ImageButton = findViewById(R.id.imageView)
-
-        imageButton.setOnClickListener{
-
-            var intent=Intent(this, MainActivity2::class.java )
+            var intent=Intent(this, MainActivity::class.java)
             startActivity(intent)
         }
+
+
     }
 }
